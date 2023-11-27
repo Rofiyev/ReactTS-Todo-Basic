bccb325af501c047fc0f607950463bc500378299
Rof1yev <rofiyevdilshod@gmail.com>
2023-11-27 22:38:25 +0500

    full

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState } from "react";
+import "./App.css";
+import { IData } from "./interfaces";
+import { data } from "./constants";
 
-function App() {
+function App(): JSX.Element {
+  const [title, setTitle] = useState<string>("");
+  const [arr, setArray] = useState<IData[]>(data);
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (): void => {
+    if (!title.trim()?.length) return;
+
+    const newData = {
+      title: title,
+      id: new Date().getTime(),
+    };
+    setArray([newData, ...arr]);
+    setTitle("");
+  };
+
+  const deleteItem = (id: number): void => {
+    setArray(arr.filter((item) => item.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <h1 className="title">Todo App</h1>
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="Enter todo"
+          value={title}
+          onChange={changeHandler}
+          className="input"
+        />
+        <button onClick={handleSubmit} className="btn">
+          Add
+        </button>
+      </div>
+      <div className="item-cards">
+        {arr.map((item) => (
+          <div key={item.id} className="item-card">
+            <h4>{item.title}</h4>
+            <div className="delBtn">
+              <button onClick={() => deleteItem(item.id)}>del</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+import { IData } from "../interfaces";
+
+export const data: IData[] = [
+  { title: "Task 1", id: 1 },
+  { title: "Task 2", id: 2 },
+  { title: "Task 3", id: 3 },
+];
